c52e9e65b6872a49126656c3a96bdb499ea346e9
99mac99 <86948705+99mac99@users.noreply.github.com>
2021-10-18 17:44:45 +0200

    add sorting

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,33 +1,32 @@
+import React from 'react';
 
-import React from "react";
- 
 const ListGroup = ({
-  selectedItem,
-  items,
-  onItemSelect,
-  textProperty,
-  valueProperty
+	selectedItem,
+	items,
+	onItemSelect,
+	textProperty,
+	valueProperty,
 }) => {
-  return (
-    <div className="list-group pt-5">
-      {items.map(item => (
-        <button
-          key={item[valueProperty]}
-          className={
-            item === selectedItem ? "list-group-item active" : "list-group-item"
-          }
-          onClick={() => onItemSelect(item)}
-        >
-          {item[textProperty]}
-        </button>
-      ))}
-    </div>
-  );
+	return (
+		<div className='list-group pt-5'>
+			{items.map((item) => (
+				<button
+					key={item[valueProperty]}
+					className={
+						item === selectedItem ? 'list-group-item active' : 'list-group-item'
+					}
+					onClick={() => onItemSelect(item)}
+				>
+					{item[textProperty]}
+				</button>
+			))}
+		</div>
+	);
 };
 
 ListGroup.defaultProps = {
-  textProperty: "name",
-  valueProperty: "_id"
+	textProperty: 'name',
+	valueProperty: '_id',
 };
 
 export default ListGroup;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -13,10 +13,10 @@ class Movies extends Component {
 		genres: [],
 		currentPage: 1,
 		pageSize: 4,
-		sortColumn: { path: 'title', order: 'asc'}
+		sortColumn: { path: 'title', order: 'asc' },
 	};
 	componentDidMount() {
-		const genres = [{ _id:"", name: 'All Genres' }, ...getGenres()];
+		const genres = [{ _id: '', name: 'All Genres' }, ...getGenres()];
 		this.setState({ movies: getMovies(), genres });
 	}
 
@@ -41,20 +41,11 @@ class Movies extends Component {
 		this.setState({ selectedGenre: genre, currentPage: 1 });
 	};
 
-	handleSort = path => {
-		const sortColumn = {...this.state.sortColumn};
-		if ( sortColumn.path === path)
-			sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : "asc";
-			else {
-				sortColumn.path = path;
-				sortColumn.order = 'asc';
-			}
-		this.setState({sortColumn });
-	}
+	handleSort = (sortColumn) => {
+		this.setState({ sortColumn });
+	};
 
-	render() {
-		const { length: count } = this.state.movies;
-
+	getPagedData = () => {
 		const {
 			pageSize,
 			currentPage,
@@ -63,18 +54,26 @@ class Movies extends Component {
 			movies: allMovies,
 		} = this.state;
 
-		if (count === 0) return <p>There are no movies in the database.</p>;
-
 		const filtered =
 			selectedGenre && selectedGenre._id
 				? allMovies.filter((m) => m.genre._id === selectedGenre._id)
 				: allMovies;
 
-
-
-		const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
+		const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
 		const movies = paginate(sorted, currentPage, pageSize);
+
+		return { totalCount: filtered.length, data: movies };
+	};
+
+	render() {
+		const { length: count } = this.state.movies;
+
+		const { pageSize, currentPage, sortColumn } = this.state;
+
+		if (count === 0) return <p>There are no movies in the database.</p>;
+
+		const { totalCount, data: movies } = this.getPagedData();
 
 		return (
 			<div className='row'>
@@ -86,14 +85,16 @@ class Movies extends Component {
 					/>
 				</div>
 				<div className='col'>
-					<p>Showing {filtered.length} movies in the database.</p>
+					<p>Showing {totalCount} movies in the database.</p>
 					<MoviesTable
 						movies={movies}
+						sortColumn={sortColumn}
 						onLike={this.handleLike}
 						onDelete={this.handleDelete}
+						onSort={this.handleSort}
 					/>
 					<Pagination
-						itemsCount={filtered.length}
+						itemsCount={totalCount}
 						pageSize={pageSize}
 						currentPage={currentPage}
 						onPageChange={this.handlePageChange}

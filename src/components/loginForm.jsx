af5ce58f8d2ddcc66516d88f363e0e53fd4063fb
99mac99 <86948705+99mac99@users.noreply.github.com>
2021-10-20 20:36:29 +0200

    add disable login button

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,82 +1,37 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
-import Input from './common/input';
+import Form from './common/form'
 
-class LoginForm extends Component {
+
+class LoginForm extends Form {
+
 	state = {
-		account: { username: '', password: '' },
-		errors: {},
-	};
+		data: { username: "", password: "" },
+		errors: {}
+	  };
+	
+	  schema = {
+		username: Joi.string()
+		  .required()
+		  .label("Username"),
+		password: Joi.string()
+		  .required()
+		  .label("Password")
+	  };
 
-	schema = {
-		username: Joi.string().requierd(),
-		password: Joi.string().requierd(),
+
+	doSubmit = () => {
+		console.log('Submitted');
 	}
 
-	validate = () => {
-		const errors = {};
-
-		const { account } = this.state;
-		if (account.username.trim() === '')
-			errors.username = 'Username is requierd.';
-		if (account.password.trim() === '')
-			errors.password = 'Password is requierd.';
-
-		return Object.keys(errors).length === 0 ? null : errors;
-	};
-
-	handleSubmit = (e) => {
-		e.preventDefault();
-
-		const errors = this.validate();
-		console.log(errors);
-		this.setState({ errors: errors || {} });
-		if (errors) return;
-
-		console.log('Submitted');
-	};
-
-	validateProperty = ({ name, value }) => {
-		if (name === 'username') {
-			if (value.trim() === '') return 'Username is required.';
-		}
-		if (name === 'password') {
-			if (value.trim() === '') return 'Password is required.';
-		}
-	};
-
-	handleChange = ({ currentTarget: input }) => {
-		const errors = { ...this.state.errors };
-		const errorMessage = this.validateProperty(input);
-		if (errorMessage) errors[input.name] = errorMessage;
-		else delete errors[input.name];
-		const account = { ...this.state.account };
-		account[input.name] = input.value;
-		this.setState({ account, errors });
-	};
-
 	render() {
-		const { account, errors } = this.state;
 		return (
 			<div>
 				<h1> Login </h1>
 				<form onSubmit={this.handleSubmit}>
-					<Input
-						name='username'
-						value={account.username}
-						label='Username'
-						onChange={this.handleChange}
-						error={errors.username}
-					/>
-					<Input
-						name='password'
-						value={account.password}
-						label='Password'
-						onChange={this.handleChange}
-						error={errors.password}
-					/>
-
-					<button className='btn btn-primary'>Login</button>
+					{this.renderInput('username', 'Username')}
+					{this.renderInput('password', 'Password', 'password')}
+					{this.renderButton('Login')}
 				</form>
 			</div>
 		);
